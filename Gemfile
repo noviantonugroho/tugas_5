2418e9c91d0b36e0197e9d03d204d90009b1b70d
noviantonugroho <noviantonugroho1989@gmail.com>
2014-12-10 10:00:57 +0700

    first commit

---
+++ Gemfile
@@ -0,0 +1,19 @@
+source 'https://rubygems.org'
+
+
+gem 'rails', '4.0.1'
+gem 'pg'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'haml', '~> 4.0.6'
+gem 'bootstrap-sass', '~> 3.3.1.0'
+gem 'simple_form', '~> 3.1.0'
+gem 'devise', '~> 3.4.1'
+gem 'paperclip', '~> 4.2.0'
+group :doc do
+  gem 'sdoc', require: false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinBoard::Application.config.secret_key_base = '3a9fddb838f1fe7a080f9900b51a2cb8cf55a6150a6536d6cd5c9ca1721c5b29011b26a01aa28f40cb4aa9820bc719aa5aff362caca42ad8604aea30a90c7fca'
